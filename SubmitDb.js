451ac680b028ecaf88318d58716c7b1bb72bd4ba
alexbponder@gmail.com <alexbponder@gmail.com>
2018-04-06 20:28:15 -0700

    updated ids

--- SubmitDb.js
+++ SubmitDb.js
@@ -3,18 +3,22 @@ $("#add-user").on("click", function(event) {
   event.preventDefault();
 
   // Grabbed values from text boxes
-  name = $("#name-input").val().trim();
-  email = $("#email-input").val().trim();
-  age = $("#age-input").val().trim();
-  comment = $("#comment-input").val().trim();
+  name = $("#nameInput").val().trim();                  //nameInput
+  role = $("#roleInput").val().trim();                  //roleInput
+  startDate = $("#startDateInput").val().trim();       //startDateInput
+  monthlyRate = $("#monthlyRateInput").val().trim();   //monthlyRateInput
 
   // Code for handling the push
   database.ref().push({
     name: name,
-    email: email,
-    age: age,
-    comment: comment,
+    role: role,
+    startDate: startDate,
+    monthlyRate: monthlyRate,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 
 });
+
+
+
+
